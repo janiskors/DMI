1bb87d0ca9b1c612ff9d3ddfdc9ac6a7c407f013
janisk@adati.lv <janiskorsukovs@gmail.com>
2017-09-27 16:08:57 +0300

    20170927_14_10

--- shell_examples.sh
+++ shell_examples.sh
@@ -1,10 +1,44 @@
 #!/bin/bash
 
+#7.uzdevums
+vertiiba='let 2 + 2'
+echo "Summas vertiba: $skaitiliska_vertiba"
+
+
+#6.uzd
+#echo $*
+#echo "__________"
+#kartas_numurs=1
+#for arguments in $*
+#do
+#echo $kartas_numurs" . arguments - "$arguments
+#kartas_numurs=$kartas_numurs+
+#done
+
+#5.uzd
+#NAME="Janis Korsukovs"
+#echo "Skriptam nodoto argumentu skaits: " $#
+#echo "Argumentu saraksts: " $*
+#echo "Argumentu saraksts (Attēlošana saraksts): " $@
+#echo "Pirmā argumenta vērtība"$1
+#echo "Otra argumenta vērtība"$2
+
+
+#4.uzd
+#NAME="Janis Korsukovs"
+#echo "skripta faila nosaukums: " $0
+#echo "Skriptam nodoto argumentu skaits: " $#
+#echo "Skripta nodoto argumentu skaits" $*
+#echo "Argumentu saraksts (Attēlošana saraksts): " $@
+#echo "Iepriekšējās izpildes rezultats: " $?
+#echo "Skripta procesa nummurs: " $$
+#echo $!
+
 #3.uzd
-NAME="Janis Korsukovs"
-echo $NAME
-unset NAME
-echo $NAME
+#NAME="Janis Korsukovs"
+#echo $NAME
+#unset NAME
+#echo $NAME
 
 #2.uzd
 #NAME="Janis Korsukovs"

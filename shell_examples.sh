78692cfb0cf4e12589a9047f8ccfcbf55d6a594a
janisk@adati.lv <janiskorsukovs@gmail.com>
2017-09-27 14:07:35 +0300

    20170927_14_10

---
+++ shell_examples.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#3.uzd
+NAME="Janis Korsukovs"
+echo $NAME
+unset NAME
+echo $NAME
+
+#2.uzd
+#NAME="Janis Korsukovs"
+#readonly NAME
+#echo $NAME
+#NAME="Korsukovs Janis"
+#echo $NAME
+
+#1.uzd
+#NAME="Janis Korsukovs"
+#echo $NAME
+
+
+#history > history_20130927.txt
+

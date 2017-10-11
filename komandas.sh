898312260f4db399f887aa25263f3c9ae595b98b
janisk@adati.lv <janisk@adati.lv>
2017-10-11 15:26:46 +0300

    20171011_15_26

---
+++ komandas.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+#1. Piemers
+val11=`expr 2 + 2`
+echo "2 + 2 rezultats : $val11"
+
+val12=`expr 2 + 2`
+echo "2 + 2 rezultats : $val12"
+
+val=13`expr 2 + 2`
+echo "2 + 2 rezultats : $val13"
+
+val=14`expr 2 + 2`
+echo "2 + 2 rezultats : $val14"
+
+val=15`expr 2 + 2`
+echo "2 + 2 rezultats : $val15"
+
+val=16`expr 2 + 2`
+echo "2 + 2 rezultats : $val16"
+
+
